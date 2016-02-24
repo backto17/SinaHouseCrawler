b2017f1335d350e34f2236f6ee2b625d2407caad
alex lin <lingang_upc@163.com>
2016-02-24 14:31:58 +0800

    start

---
+++ sinahouse/items.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class HouseProjectItem(scrapy.Item):
+    # define the Fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+class HouseItem(scrapy.Item):
+    save_path = scrapy.Field()
+    source_id = scrapy.Field()
+    index_url = scrapy.Field()
+    community_id = scrapy.Field()
+    community_name = scrapy.Field()
+    city_id = scrapy.Field() 
+    city_name = scrapy.Field()   
+    area_id = scrapy.Field()
+    area_name = scrapy.Field() 
+    address = scrapy.Field() 
+    longitude = scrapy.Field()
+    latitude = scrapy.Field()
+    developer = scrapy.Field() 
+    community_desc = scrapy.Field()
+    property_manage_fee = scrapy.Field() 
+    property_manage_com = scrapy.Field() 
+    bus = scrapy.Field() 
+    metro = scrapy.Field() 
+    cover_id = scrapy.Field()   
+    open_date = scrapy.Field()  
+    check_in_date = scrapy.Field()
+    house_img_urls = scrapy.Field()
+    cover_urls = scrapy.Field()   
+    household_size = scrapy.Field()
+    per_square_price = scrapy.Field()
+    decoration = scrapy.Field()
+    property_type = scrapy.Field()
+    construction_area = scrapy.Field()
+    
+    
+    
---
+++ sinahouse/middlewares.py
@@ -0,0 +1,32 @@
+import random
+import logging
+
+class UserAgentMiddleware(object):   
+    """add a random useragent to request"""  
+      
+    def __init__(self, agents):
+        self.agents = agents
+    
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler.settings.getlist('USER_AGENTS'))
+    
+    def process_request(self, request, spider):
+        request.headers.setdefault('User-Agent', random.choice(self.agents))
+
+class ProxyMiddleware(object):
+    """add a random proxy to request"""
+    
+    def __init__(self, proxies):
+        self.proxies =  proxies
+    
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler.settings.getlist('PROXIES'))
+    
+    def process_request(self, request, spider):
+        request.meta['proxy'] = random.choice(self.proxies)
+        print request.meta
+        
+        
+           
