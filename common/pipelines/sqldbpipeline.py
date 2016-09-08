68f83ae54962f85b545021de34e0f332365d83a8
alex lin <alric.lin.china@gmail.com>
2016-09-08 23:15:14 +0800

    add sqldbpipeline

---
+++ common/pipelines/sqldbpipeline.py
@@ -0,0 +1,54 @@
+# coding: utf-8
+'''
+@date: 2016-09-08
+
+@author: LG
+'''
+import abc
+
+from twisted.enterprise import adbapi
+
+class AsyncSqlPipelineBase(object):
+    """
+    class:异步写入关系型数据库的基础管道,
+    支持DB-API 2.0 compliant database
+    """
+    __metaclass__ = abc.ABCMeta
+    
+    def __init__(self, database_info, stats):
+        """
+        para: database_info 应该包含对应数据库的连接信息
+                    例如：database_info = {
+            'dbapiName': 'MySqldb',
+            'host' : '192.168.3.238',
+            'db' : 'test',
+            'user' : 'alex',
+            'passwd' : 'alex',
+#             'cursorclass' : MySQLdb.cursors.DictCursor,
+            'charset' : 'utf8',
+            'use_unicode' : True
+            }
+        """
+        self.dbpool = adbapi.ConnectionPool(**database_info)
+        self.stats = stats
+    
+    @classmethod
+    def from_crawler(cls, crawler):
+        """
+        func:从当前爬虫配置文件获取数据库连接所需信息
+        """
+        database_info = crawler.settings.get('DATABASE_INFO')
+        stats = crawler.stats
+        return cls(database_info, stats)
+    
+    @abc.abstractmethod
+    def process_item(self,item, spider):
+        pass
+    
+    def close_spider(self,spider):
+        """
+        func: 关闭数据库连接
+        """
+        self.dbpool.close()
+        
+    
