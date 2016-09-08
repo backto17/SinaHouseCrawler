40ad0ea14d0e6982f6492773660c598704b38b4e
alric.lin <alric.lin.china@gmail.com>
2016-09-08 16:03:10 +0800

    start to refactor

--- common/items/__init__.py
+++ common/items/__init__.py
@@ -3,3 +3,8 @@
 @date: 2016-09-08
 @author: alex.lin
 '''
+
+from .baseitem import BaseItem
+
+
+__all__ = ['BaseItem',]
---
+++ common/items/baseitem.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+'''
+@date: 2016-09-08
+@author: alex.lin
+'''
+from scrapy.item import Item, Field
+
+class BaseItem(Item):
+    """
+    基础item, 包含通用属性
+    """
+    url = Field() # 来源url
+    project = Field() #  工程名字
+    spider = Field() # 爬虫名字
+    server = Field() # 服务器
+    date = Field() # 爬取时间
