861e51a9147dc8f66a2858ad94330de90350bd09
alric.lin <alric.lin.china@gmail.com>
2016-09-09 11:48:29 +0800

    refactor to use sqlpipelinebase

--- sinahouse/pipelines.py
+++ sinahouse/pipelines.py
@@ -14,6 +14,7 @@ from scrapy.mail import MailSender
 from scrapy.exceptions import DropItem
 from scrapy.pipelines.images import ImagesPipeline
 import requests
+from common.pipelines import AsyncSqlPipelineBase
 
 from utils import semaphore_thread
 
@@ -42,42 +43,40 @@ class MongoPipeline(object):
 #         mailer = MailSender.from_settings(self.settings)        
 #         mailer.send(to=["lingang@xxx.com"], subject="THANKS", body="THANKS YOU!",cc=[])
 
-class MySQLPipeline(object):
+class MySQLPipeline(AsyncSqlPipelineBase):
     '''
-    数据写入 mysql的管道
+    func:数据写入 mysql的管道
     '''
-    def __init__(self, stats, mysql_info):
-        self.dbpool = adbapi.ConnectionPool('MySQLdb', **mysql_info)
-        self.stats = stats
-        self.logger = logging.getLogger(__name__)
-        
-    @classmethod
-    def from_crawler(cls, crawler):
-        '''
-        获取数据库连接信息
-        '''
-        mysql_info = crawler.settings.get('MYSQL_INFO')
-        stats = crawler.stats
-        return cls(stats, mysql_info)
-        
     def process_item(self, item, spider):
         query = self.dbpool.runInteraction(self._insert, item)
         query.addErrback(self._handle_error)
         return item
     
-    def _insert(self, tx, item):
-        tx.execute(""" 
+    def _insert(self, cursor, item):
+        """
+        func: 插入数据库,
+                    建表信息如下:
+        CREATE TABLE `house_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `house_name` varchar(300) DEFAULT NULL,
+  `house_index_url` varchar(300) DEFAULT NULL,
+  `house_price` varchar(100) DEFAULT NULL,
+  `house_address` varchar(500) DEFAULT NULL,
+  `longtitude_latitude` varchar(100) DEFAULT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+)
+        """
+        cursor.execute(""" 
         insert into  house_info(house_name,house_index_url,house_price,house_address,longtitude_latitude)
-        values (%s,%s,%s,%s,%s)""",(item['house_name'],item['house_index_url'],item['house_price'],item['house_address'],item['longtitude_latitude']))
+        values (%s,%s,%s,%s,%s)""",(item['community_name'],item['index_url'],item['per_square_price'],item['address'],item['longtitude_latitude']))
         
         self.stats.inc_value('mysql_items_added', count=1, start=0)
              
     def _handle_error(self,e):
         self.stats.inc_value('mysql_items_failed', count=1, start=0)
         self.logger.error(e)
-    
-    def close_spider(self,spider):
-        self.dbpool.close()
+
         
 class ImagePipeline(object):
     """
@@ -122,7 +121,7 @@ class ImagePipeline(object):
             
 class CustomImagesPipeline(ImagesPipeline):
     """
-    继承scrapy自带ImagesPipeline,
+    func:继承scrapy自带ImagesPipeline,
     实现自己的功能需求
     """
     def get_media_requests(self, item, info):
