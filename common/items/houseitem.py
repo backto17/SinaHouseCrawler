b7c5282773bff9dd13345be9548b2ce2213f0718
alric.lin <alric.lin.china@gmail.com>
2016-09-18 17:54:27 +0800

    redesign img_info

--- common/items/houseitem.py
+++ common/items/houseitem.py
@@ -17,12 +17,13 @@ class HouseItemBase(BaseItem):
     open_date = scrapy.Field() # 开盘时间
     checkin_date = scrapy.Field() # 入住时间
     address = scrapy.Field() # 地址(北京某某区某某路...)
-    longtitude_latitude = scrapy.Field() # 经度 纬度
+    lon_lat = scrapy.Field() # 经度 纬度
     developer = scrapy.Field() # 开发商
     property_company = scrapy.Field() # 物业公司
     property_manage_fee = scrapy.Field() # 物业费
     decoration = scrapy.Field() # 装修情况
-    cover_info = scrapy.Field() # 楼盘封面图片
+    cover_url = scrapy.Field() # 楼盘封面图片
+    cover_path = scrapy.Field() # 楼盘封面图片的保存路径
     layout_items = scrapy.Field() # 户型信息
     
 class HouseLayoutItemBase(BaseItem):
@@ -31,5 +32,6 @@ class HouseLayoutItemBase(BaseItem):
     """
     name = scrapy.Field() # 户型名称
     area = scrapy.Field() # 户型面积
-    img_info = scrapy.Field() # 户型图片
+    img_url = scrapy.Field() # 户型图片
+    img_path = scrapy.Field() # 户型图片的保存路径
     
