2d22d9ab52362d1f3b1a990ab35561722aa7e883
alric.lin <alric.lin.china@gmail.com>
2016-09-18 17:57:57 +0800

    add manage_fee and modify img_info

--- sinahouse/spiders/housespider.py
+++ sinahouse/spiders/housespider.py
@@ -4,6 +4,7 @@
 @author: alex.lin
 '''
 import re
+import datetime
 
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
@@ -33,18 +34,21 @@ class SinaHouseSpider(CrawlSpider):
         func:提取楼盘信息
         """
         item = SinaHouseItem()
-        item['source_id'] = settings.SOURCE
+        item['create_time'] = datetime.datetime.now()
+        item['source_id'] = re.search(r'.*?(?P<source_id>\d{4,})', response.url).groupdict('source_id')['source_id']
         item['name'] = response.xpath('//h1/text()').extract_first()
-        item['price'] = response.xpath("//*[@id='callmeBtn']/ul/li[1]/em[1]/text()").extract_first(default=u'未知').strip()
+        item['price'] = ''.join(response.xpath("//*[@id='callmeBtn']/ul/li[1]/em[1]/text() | //*[@id='callmeBtn']/ul/li[1]/em[2]/text() ").extract()) or u'未知'
         item['url'] = response.url
         item['open_date'] = '-'.join(re.findall('(\d+)',response.xpath(u"//*[@id='callmeBtn']/ul/li[4]/span[2]/text()").extract_first(default=u'待定').strip()))
         item['checkin_date'] = '-'.join(re.findall('(\d+)',response.xpath(u'(//*[@id="callmeBtn"]//div[@title])[2]/@title').extract_first(default=u'待定')))
         item['address'] = response.xpath(u'//*[@id="callmeBtn"]/ul/li[2]/span[2]/text()').extract_first(default=u'未知').strip()
-        item['longtitude_latitude'] = ','.join([response.xpath('//script').re_first(".*?coordx='(\d+.\d+)'"), response.xpath('//script').re_first(".*?coordy='(\d+.\d+)'.*")])
+        item['lon_lat'] = ','.join([response.xpath('//script').re_first(".*?coordx='(\d+.\d+)'"), response.xpath('//script').re_first(".*?coordy='(\d+.\d+)'.*")])
         item['developer'] = response.xpath(u"//div[@class='info wm']/ul/li[3]/text()").extract_first(default=u'未知').strip()
         item['property_company'] = response.xpath(u"//li[contains(span,'物业公司')]/text()").extract_first(default=u'未知').strip()
+        item['property_manage_fee'] = response.xpath("//div[@class='info wm']/ul/li[7]/text()").extract_first(default=u'未知').strip()
         item['decoration'] = response.xpath("//div[@class='info wm']/ul/li[12]/text()").extract_first(default=u'未知').strip()
-        item['cover_info'] = {'url': response.xpath("//*[@id='con_tab1_con_4']/a/img/@lsrc").extract_first(),}
+        item['cover_url'] = response.xpath("//*[@id='con_tab1_con_4']/a/img/@lsrc").extract_first()
+        item['cover_path'] = None
         # 楼盘户型图首页
         houselayout_index_url = response.xpath('/html/body/div[1]/div[10]/ul/li[4]/a/@href').extract_first()
 
@@ -79,7 +83,8 @@ class SinaHouseSpider(CrawlSpider):
             houselayout['name'] = layout.xpath(".//div[@class='imgBox']/p/text()").extract_first(default=u'其他')
             img_url_tmp = layout.xpath('.//img/@src').extract_first()
             # 转换为大图的链接
-            houselayout['img_info'] = {'url': '%s.jpg' % img_url_tmp[:(img_url_tmp.index('mk7')+3)],}
+            houselayout['img_url'] = '%s.jpg' % img_url_tmp[:(img_url_tmp.index('mk7')+3)]
+            houselayout['img_path'] = None
             houselayout['area'] = layout.xpath("./p[1]/em/text()").extract_first(default=0)
             item['layout_items'].append(dict(houselayout))
         
