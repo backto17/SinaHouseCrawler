92eb99a57b5a0cf73847a324d9f7ac6d54f15a28
alric.lin <alric.lin.china@gmail.com>
2016-09-28 19:38:45 +0800

    new proxy to suport https

--- common/middlewares/proxy.py
+++ common/middlewares/proxy.py
@@ -15,15 +15,45 @@ class ProxyMiddleware(object):
     """add a random proxy to request"""
     
     def __init__(self, proxies):
-        self.proxies = set(proxies)
-    
+        self.proxies = {'http': set(),
+                        'https':set(),}
+        for proxy in proxies:
+            self.proxies['http'].add(proxy.get('http'))
+            self.proxies['https'].add(proxy.get('https'))
+            
+        self.proxies['http'].discard(None)
+        self.proxies['https'].discard(None)
+        
     @classmethod
     def from_crawler(cls, crawler):
         return cls(crawler.settings.getlist('PROXIES'))
     
     def process_request(self, request, spider):
-        if 'proxy' in request.meta and request.meta['proxy'] in self.proxies:
-            logger.warning('Bad proxy: %s', request.meta['proxy'])
-            self.proxies.remove(request.meta['proxy'])
-        if self.proxies:
-            request.meta['proxy'] = random.sample(self.proxies,1)[0]
+        scheme = self.get_scheme(request.url)
+        if 'proxy' in request.meta and not 'proxy_filed_time' in request.meta:
+            request.meta['proxy_filed_time'] = 1
+        elif 'proxy_filed_time' in request.meta:
+            if request.meta['proxy_filed_time'] >= 2:
+                logger.warning('Bad %s proxy: %s', scheme, request.meta['proxy'])
+                self.remove_bad_proxy(scheme, request.meta['proxy'])
+            else:
+                request.meta['proxy_filed_time'] += 1
+                return
+        proxy = self.get_proxy(scheme)
+        if proxy:
+            request.meta['proxy'] = proxy
+                
+    @staticmethod
+    def get_scheme(url):
+        if url.startswith('http:'):
+            return 'http'
+        else:
+            return 'https'
+    
+    def remove_bad_proxy(self, scheme, proxy):
+        proxy = proxy[proxy.rindex('/')+1:]
+        self.proxies[scheme].discard(proxy)
+    
+    def get_proxy(self,scheme):
+        if self.proxies[scheme]:
+            return '%s://%s' %(scheme, random.sample(self.proxies[scheme],1)[0])
