9fa15ab38a7787238342eef3127c1abcf880bf44
alric.lin <alric.lin.china@gmail.com>
2016-09-29 16:20:54 +0800

    add spider xici

---
+++ proxy/proxy/spiders/xici.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+'''
+@date: 2016-09-28
+@author: alex.lin
+'''
+
+from scrapy.spiders import Spider
+from scrapy.http.request import Request
+
+
+
+class XiciSpider(Spider):
+    """ meta={'proxy': 'http://127.0.0.1:1080'}, """
+    name = 'xici'
+    allowed_domains = ['www.xicidaili.com']
+    start_urls = ['http://www.xicidaili.com/nn/']
+    custom_settings = {
+                       'DOWNLOADER_MIDDLEWARES': {
+                                                  'common.middlewares.useragent.UserAgentMiddleware': 730,
+                                                  'common.middlewares.proxy.ProxyMiddleware': 735,
+                                                  },
+                       'ITEM_PIPELINES': {},
+                       'DOWNLOAD_TIMEOUT': 30,
+                       'RETRY_ENABLED' : True,
+                       }
+    def start_requests(self):
+        for i in xrange(1, 10):
+            yield Request('http://www.xicidaili.com/nn/%s' % i, callback=self.parse)
+        
+    def parse(self, response):
+        ip_records = response.xpath("//*[@id='ip_list']//tr")
+        for ip_record in ip_records[1:]:
+            ip = ip_record.xpath('./td[2]/text()').extract_first()
+            port = ip_record.xpath('./td[3]/text()').extract_first()
+            proxy_type = ip_record.xpath('./td[6]/text()').extract_first().lower()
+            if proxy_type == 'http':
+                yield Request('http://www.163.com/',dont_filter=True, 
+                              meta={'item': {proxy_type: '%s:%s' % (ip, port),},
+                                    'proxy':'%s://%s:%s' % (proxy_type, ip, port),
+                                    'dont_retry': True,
+                                    },
+                              callback=self.examined_item)
+    
+    def examined_item(self,response):
+        item = response.meta['item']
+        yield item
