89e83fbddd180b6c01565172935ff1f917f3c9e2
alric.lin <alric.lin.china@gmail.com>
2017-02-10 17:37:49 +0800

    fixed huxing

--- house/house/spiders/sinahouse.py
+++ house/house/spiders/sinahouse.py
@@ -20,7 +20,7 @@ class SinaHouseSpider(CrawlSpider):
 
     name = 'leju'
     allowed_domains = ['leju.com', ]
-    start_urls = ['http://sh.leju.com/', ]
+    start_urls = ['http://SC.leju.com/', ]
     rules = [
         #  具体楼盘链接提取
         Rule(LinkExtractor(allow=(r'.*//house\.leju\.com/\w+\d+/(#wt_source=.*)?$')),
@@ -48,7 +48,8 @@ class SinaHouseSpider(CrawlSpider):
             u"//ul[@class='b_list_02 clearfix conClassName']/li[1]//img/@lsrc").extract_first()
         house['cover_path'] = None
         # 去详情页获取楼盘详情
-        detail_url = response.xpath(u"//ul[@class='z_main_menu']//a[contains(text(),'楼盘详情')]/@href").extract_first()
+        detail_url = response.urljoin(
+            response.xpath(u"//ul[@class='z_main_menu']//a[contains(text(),'楼盘详情')]/@href").extract_first())
         if detail_url:
             yield scrapy.Request(detail_url, meta={'house': house}, callback=self.parse_house_detail)
         else:
@@ -71,40 +72,25 @@ class SinaHouseSpider(CrawlSpider):
             u"//ul[@class='z_info_list clearfix']//li[contains(label, '入住时间')]/p/text()").extract_first(u'待定').strip()
         house['address'] = response.xpath(
             u"//ul[@class='z_info_list clearfix']//li[contains(label, '项目地址')]/p/text()").extract_first(u'未知').strip()
-        house['lon_lat'] = ','.join([response.xpath('//script').re_first(".*?coordx='(\d+.\d+)'"), response.xpath(
-            '//script').re_first(".*?coordy='(\d+.\d+)'.*")])
+        # house['lon_lat'] = ','.join([response.xpath('//script').re_first(".*?coordx='(\d+.\d+)'"), response.xpath(
+        #     '//script').re_first(".*?coordy='(\d+.\d+)'.*")])
         house['developer'] = response.xpath(
             ur"//ul[@class='z_info_list clearfix']//li[contains(label,'\u5f00\u2002\u53d1\u2002\u5546\uff1a')]/p/text()").extract_first(
             u'未知').strip()
         house['property_company'] = response.xpath(
             u"//ul[@class='z_info_list clearfix']//li[contains(label, '物业公司')]/p/text()").extract_first(u'未知').strip()
         house['property_fee'] = response.xpath(
-            u"//ul[@class='z_info_list z_info_list01 clearfix']//li[contains(label,'\u7269\u2002\u4e1a\u2002\u8d39\uff1a')]").extract_first(
+            u"//ul[@class='z_info_list z_info_list01 clearfix']//li[contains(label,'\u7269\u2002\u4e1a\u2002\u8d39\uff1a')]/text()").extract_first(
             u'未知').strip()
         house['decoration'] = response.xpath(
-            u"//ul[@class='z_info_list clearfix']//li[contains(label, '装修情况')]/text()").extract_first().strip()
-
+            u"//ul[@class='z_info_list clearfix']//li[contains(label, '装修情况')]/text()").extract_first(u'未知').strip()
+        house['description'] = response.xpath("//div[@class='z_project_info']/text()").extract_first(u'暂无').strip()
         # 楼盘户型图首页
-        houselayout_index_url = response.xpath('/html/body/div[1]/div[10]/ul/li[4]/a/@href').extract_first()
-
-        if houselayout_index_url:
-            yield scrapy.Request(url=houselayout_index_url, meta={"house": house},
-                                 callback=self.parse_houselayout_index)
-        else:
-            self.logger.info(u'此楼盘无图片: %s', house['url'])
-            yield house
-
-    def parse_houselayout_index(self, response):
-        """
-        func:楼盘户型信息处理
-        """
-        house = response.meta["hosue"]
-        house['layout_items'] = []
-        houselayout_index_url = response.xpath(
-            u"//div[@class='housingNav w']//li[contains(a,'户型图')]/a/@href").extract_first()
-        if houselayout_index_url:
-            self.logger.debug(u'楼盘图片首页: %s', houselayout_index_url)
-            yield scrapy.Request(url=houselayout_index_url, meta={'house': house}, callback=self.parse_houselayout)
+        huxing_url = response.xpath(u"//ul[@class='z_main_menu']//a[contains(text(),'户型图')]/@href").extract_first()
+        if huxing_url:
+            self.logger.debug(u'楼盘图片首页: %s', huxing_url)
+            house['layout_items'] = []
+            yield scrapy.Request(url=response.urljoin(huxing_url), meta={'house': house}, callback=self.parse_houselayout)
         else:
             self.logger.info(u'此楼盘无户型图片: %s', house['url'])
             yield house
@@ -113,25 +99,26 @@ class SinaHouseSpider(CrawlSpider):
         """
         func:户型信息处理
         """
-        house = response.meta['house_house']
-        layout_records = response.xpath("//ul[@class='sFy03 clearfix']//li")
-        for layout in layout_records:
+        house = response.meta['house']
+        huxing_records = response.xpath("//ul[@class='b_list01 clearfix']//li")
+        for huxing_record in huxing_records:
             houselayout = SinaHouseLayout()
-            houselayout['name'] = layout.xpath(".//div[@class='imgBox']/p/text()").extract_first(default=u'其他')
-            img_url_tmp = layout.xpath('.//img/@src').extract_first()
-            # 转换为大图的链接
-            houselayout['img_url'] = '%s.jpg' % img_url_tmp[:(img_url_tmp.index('mk7') + 3)]
+            houselayout['name'] = huxing_record.xpath(".//h2/span/text()").extract_first('未知').strip()
+            img_url_tmp = huxing_record.xpath(".//img/@lsrc").extract_first()
+            # 小图: http://src.leju.com/imp/imp/deal/a6/39/c/9bf2cb7d253475993fbce5a4c65_p7_mk7_cm358X269_wm47_pt1.jpg
+            # 转换为大图的链接: http://src.leju.com/imp/imp/deal/a6/39/c/9bf2cb7d253475993fbce5a4c65_p7_mk7_wm47_pt1.jpg
+            houselayout['img_url'] = img_url_tmp[:(img_url_tmp.index('mk7') + 3)] + "_wm47_pt1.jpg"
             houselayout['img_path'] = None
-            houselayout['area'] = layout.xpath("./p[1]/em/text()").extract_first(default=0)
-            houselayout['price'] = '%s%s' % (
-                layout.xpath('./p[2]/em/text()').extract_first(''), layout.xpath('./p[2]/text()[2]').extract_first(''))
+            houselayout['area'] = huxing_record.xpath(".//h3[1]/text()").extract_first()
+            houselayout['price'] = huxing_record.xpath(
+                "concat(.//h3[2]/text()[1],.//h3[2]/text()[2])").extract_first().strip()
             house['layout_items'].append(dict(houselayout))
-
-        # 部分户型图有分页, 如: http://data.house.sina.com.cn/sc127009/huxing/#wt_source=data6_tpdh_hxt       
-        next_url = response.xpath(u"//div[@class='sPageBox']//a[contains(text(),'下一页')]/@href").extract_first()
+        # 部分户型图有分页, 如: http://data.house.sina.com.cn/sc127009/huxing/#wt_source=data6_tpdh_hxt
+        # 网站改版后新链接: http://house.leju.com/sc127009/huxing/
+        next_url = response.xpath(u"//a[@class='next']/@href").extract_first()
         if next_url:
             self.logger.debug(u'进入楼盘户型图下一页: %s', next_url)
-            yield scrapy.Request(url=next_url, meta={'house': house,}, callback=self.parse_houselayout)
+            yield scrapy.Request(url=response.urljoin(next_url), meta={'house': house,}, callback=self.parse_houselayout)
         else:
             self.logger.info(u'此楼盘链接处理完毕: %s', house['url'])
             yield house
